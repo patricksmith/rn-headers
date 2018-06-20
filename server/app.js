ca8e7d4a84546bc08f531c832bb79741d78a3eb4
Patrick Smith <patrick@promptworks.com>
2018-06-19 21:04:14 -0400

    Throw some <> on those links

--- server/app.js
+++ server/app.js
@@ -6,9 +6,9 @@ app.get('/', (req, res) => {
   const { page = 1 } = req.query
   const next = parseInt(page) + 1
   const last = 42
-  res.append('Link', `${req.protocol}://${req.hostname}${req.originalUrl}; rel="self"`)
-  res.append('Link', `${req.protocol}://${req.hostname}${req.path}?page=${next}; rel="next"`)
-  res.append('Link', `${req.protocol}://${req.hostname}${req.path}?page=${last}; rel="last"`)
+  res.append('Link', `<${req.protocol}://${req.hostname}${req.originalUrl}>; rel="self"`)
+  res.append('Link', `<${req.protocol}://${req.hostname}${req.path}?page=${next};> rel="next"`)
+  res.append('Link', `<${req.protocol}://${req.hostname}${req.path}?page=${last}>; rel="last"`)
   res.send(`Here's page ${page}!`)
 })
 
